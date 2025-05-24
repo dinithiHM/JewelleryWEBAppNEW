e162e3f2e06dece084b0c09aec0ef2d0377ca996
Dinithi Mahathanthri <dinithihimesha29@gmail.com>
2025-05-24 22:28:59 +0530

    final server side changes

--- server/sql/update_order_status_enum.sql
+++
@@ -1,3 +0,0 @@
--- Update the order_status ENUM to include "Picked Up"
-ALTER TABLE custom_orders 
-MODIFY COLUMN order_status ENUM('Pending', 'In Progress', 'Completed', 'Picked Up', 'Delivered', 'Cancelled') DEFAULT 'Pending';
